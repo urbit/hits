1adc64124319668027cf14c7b6f728eede35dc88
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-03-06 14:53:29 +0000

    Image and button styling

--- ui/src/components/AppTable.jsx
+++ ui/src/components/AppTable.jsx
@@ -1,42 +1,57 @@
 export default function AppTable({ apps }) {
   console.log('AppTable apps: ', apps)
 
-    return (
-      <table>
-        <thead>
-          <th>#</th>
-          <th>ICON</th>
-          <th>INFO</th>
-          <th>DOWNLOAD ↓</th>
-        </thead>
-        <tbody>
-          {apps.map((app, index) => (
-            <tr key={index + 1}>
-              <td>
-                {index + 1}
-              </td>
-              <td >
-                {/* TODO handle null cases where there's no icon */}
-                <img
-                src={app.docket.image}
-                alt={`${app.name} app icon`}
-                />
-              </td>
-              <td>
-                <span className='app-title'>
-                  {app.name.toUpperCase()}
-                </span>
-                <span className='app-description'>
-                  {/* TODO enforce sentence case */}
-                  {app.docket.info}
-                </span>
-              </td>
-              <td>
-                <button className='get-app-button'>GET</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )
+  function normalizeUrbitColor(color) {
+    // TODO is this necessary?
+    // if (color.startsWith('#')) {
+    //   return color;
+    // }
+    return `#${color.slice(2).replace('.', '').toUpperCase()}`;
   }
+
+  return (
+    <table>
+      <thead>
+        <th>#</th>
+        <th>ICON</th>
+        <th>INFO</th>
+        <th>DOWNLOAD</th>
+      </thead>
+      <tbody>
+        {apps.map((app, index) => (
+          <tr key={index + 1}>
+            <td className='app-index'>
+              {index + 1}
+            </td>
+            <td
+              className='app-icon'
+              style={
+                app.docket.color &&
+                { backgroundColor: `${normalizeUrbitColor(app.docket.color)}` }
+              }
+            >
+              {app.docket.image &&
+              <img
+              src={app.docket.image}
+              alt={`${app.name} app icon`}
+              />
+              }
+            </td>
+            <td className='app-info'>
+              <span className='app-title'>
+                {app.name.toUpperCase()}
+              </span>
+              <span className='app-description'>
+                {/* TODO enforce sentence case */}
+                {app.docket.info}
+              </span>
+            </td>
+            <td className='app-button'>
+              <button >GET</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
